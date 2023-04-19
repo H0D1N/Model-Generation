14138007b5e2ae5a0bcb948f0ab492597588364a
H0D1N <1476950711@qq.com>
2023-04-19 17:05:57 +0800

    acc：92%

--- data/cifarDataset.py
+++ data/cifarDataset.py
@@ -1,19 +1,29 @@
 from torchvision import datasets
 from torch.utils.data import Dataset
 from torchvision import transforms
-import os
 import torch
 import random
 import numpy as np
 
 
 class cifarDataset(Dataset):
-    def __init__(self, train=True,):
+    def __init__(self, train=True):
         super(cifarDataset, self).__init__()
         self.train = train
-        self.transform = transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize(mean=np.array([125.3, 123.0, 113.9]) / 255.0,
-                                                         std=np.array([63.0, 62.1, 66.7]) / 255.0)])
+        if train:
+            self.transform = transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomRotation(15),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=np.array([125.3, 123.0, 113.9]) / 255.0,
+                                     std=np.array([63.0, 62.1, 66.7]) / 255.0)]
+            )
+        else:
+            self.transform = transforms.Compose(
+                [transforms.ToTensor(), transforms.Normalize(mean=np.array([125.3, 123.0, 113.9]) / 255.0,
+                                                             std=np.array([63.0, 62.1, 66.7]) / 255.0)])
         self.dataset = datasets.cifar.CIFAR100(root='data', train=train, transform=transforms.Compose(
             [transforms.ToTensor(), transforms.Normalize(mean=np.array([125.3, 123.0, 113.9]) / 255.0,
                                                          std=np.array([63.0, 62.1, 66.7]) / 255.0),
@@ -24,10 +34,10 @@ class cifarDataset(Dataset):
             idx = np.where(targets == i)
             self.idxs.append(idx)
         self.task_list = [i for i in range(100)]
-
-
     def __getitem__(self, idx):
+
         random.shuffle(self.task_list)
+
         self.task = [self.task_list[i] for i in range(10)]  # random.choice(self.task_list)
         prompt = torch.zeros(100).scatter_(0, torch.tensor(self.task), 1)
 
